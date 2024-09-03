7ab1b6f83e29d006a40f7f1b7cb071ad2b51c7a4
OksanaOnduke <oksanarybalko@yandex.ru>
2024-09-03 14:29:59 +0300

    Update LinkTest.java

--- Study-main/src/test/java/LinkTest.java
+++ Study-main/src/test/java/LinkTest.java
@@ -8,18 +8,14 @@ public class LinkTest extends BaseTest {
 
     @Test
     public void testLinkIsWorking() {
-        // Находим элемент ссылки по XPath
         WebElement link = driver.findElement(By.xpath("//*[@id='pay-section']/div/div/div[2]/section/div/a"));
 
-        // Получаем URL из атрибута href
         String url = link.getAttribute("href");
 
-        // Переходим по ссылке
         assert url != null;
         driver.navigate().to(url);
         System.out.println(url);
 
-        // Проверяем, что страница загрузилась успешно по заголовку
         String expectedTitle = "Порядок оплаты и безопасность интернет платежей";
         String actualTitle = driver.getTitle();
         System.out.println(actualTitle);
