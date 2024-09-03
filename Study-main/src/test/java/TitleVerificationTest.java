24fe18c5f78973160746eb82e838217f24c2e909
OksanaOnduke <oksanarybalko@yandex.ru>
2024-09-03 14:31:37 +0300

    Update TitleVerificationTest.java

--- Study-main/src/test/java/TitleVerificationTest.java
+++ Study-main/src/test/java/TitleVerificationTest.java
@@ -9,14 +9,12 @@ public class TitleVerificationTest extends BaseTest {
 
     @Test
     public void testTitleText() {
-        // Находим элемент по его тегу h2 и классу pay__wrapper
+      
         WebElement titleElement = driver.findElement(By.xpath("//div[@class='pay__wrapper']//h2"));
 
-        // Получаем текст элемента
         String titleText = titleElement.getText();
         System.out.println(titleText);
 
-        // Проверяем текст элемента, ожидаем, что он будет "Онлайн пополнение без комиссии" (с учётом переноса строки)
         assertEquals("Онлайн пополнение\nбез комиссии", titleText, "Название блока не соответствует ожидаемому!");
     }
 }
